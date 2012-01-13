d8083c1725806405575dc5a8af1c49414af4ac76
Tyler Holland <tywholland@gmail.com>
2012-01-13 14:50:05 -0800

    Initial Project Commit

---
+++ GroceryList/src/com/tyhollan/grocerylist/GroceryListActivity.java
@@ -0,0 +1,13 @@
+package com.tyhollan.grocerylist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GroceryListActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
