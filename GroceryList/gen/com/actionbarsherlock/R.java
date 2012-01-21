6a82671e6bde9b95c55f3cb77fd879e59fc3b973
Tyler Holland <tywholland@gmail.com>
2012-01-21 00:31:55 -0800

    Added icon placeholders for all current icons

--- GroceryList/gen/com/actionbarsherlock/R.java
+++ GroceryList/gen/com/actionbarsherlock/R.java
@@ -664,14 +664,16 @@ containing a value of this type.
         public static final int abs__tab_unselected_holo=0x7f020026;
         public static final int abs__tab_unselected_pressed_focused_holo=0x7f020027;
         public static final int abs__tab_unselected_pressed_holo=0x7f020028;
-        public static final int ic_launcher=0x7f020029;
-        public static final int vpi__tab_indicator=0x7f02002a;
-        public static final int vpi__tab_selected_focused_holo=0x7f02002b;
-        public static final int vpi__tab_selected_holo=0x7f02002c;
-        public static final int vpi__tab_selected_pressed_holo=0x7f02002d;
-        public static final int vpi__tab_unselected_focused_holo=0x7f02002e;
-        public static final int vpi__tab_unselected_holo=0x7f02002f;
-        public static final int vpi__tab_unselected_pressed_holo=0x7f020030;
+        public static final int app_icon=0x7f020029;
+        public static final int grocery_list_icon=0x7f02002a;
+        public static final int recipe_book_icon=0x7f02002b;
+        public static final int vpi__tab_indicator=0x7f02002c;
+        public static final int vpi__tab_selected_focused_holo=0x7f02002d;
+        public static final int vpi__tab_selected_holo=0x7f02002e;
+        public static final int vpi__tab_selected_pressed_holo=0x7f02002f;
+        public static final int vpi__tab_unselected_focused_holo=0x7f020030;
+        public static final int vpi__tab_unselected_holo=0x7f020031;
+        public static final int vpi__tab_unselected_pressed_holo=0x7f020032;
     }
     public static final class id {
         public static final int abs__action_bar=0x7f050020;
